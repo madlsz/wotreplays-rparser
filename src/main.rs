265ea1593a5252fdf5fd8a0890c90cc8abcfa80f
madlsz <dlugosz00@gmail.com>
2024-05-19 22:14:37 +0200

    loading json

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use bstr::ByteSlice;
+use serde_json::{Result, Value};
 use std::fs;
+
+struct Player {
+    id: u32,
+}
 
 fn main() {
     let path = "20240519_2045_poland-Pl21_CS_63_05_prohorovka.wotreplay";
@@ -8,9 +13,15 @@ fn main() {
     let needle_start = "[{\"personal\":";
     let needle_end = "}}]";
     let idx_start = buf.find(needle_start).expect("not  found");
-    let idx_end = buf[idx_start..].find(needle_end).expect("not  found");
+    let idx_end = buf.find_iter(needle_end).last().expect("not  found");
     println!("start: {idx_start}");
     println!("end: {idx_end}");
+
+    let buf_1 = String::from_utf8(buf[idx_start..idx_end + needle_end.len()].to_vec()).unwrap();
+
+    let v: Value = serde_json::from_str(&buf_1).unwrap();
+
+    println!("{}", v[0]);
 
     // for byte in &buf {
     //     print!("{:02x} ", byte);
