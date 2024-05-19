b7fab9e92bb15c4e7c0d5a3a0402707670b1f045
madlsz <dlugosz00@gmail.com>
2024-05-19 21:31:52 +0200

    extracting the indices of the arena section

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use bstr::ByteSlice;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let path = "20240519_2045_poland-Pl21_CS_63_05_prohorovka.wotreplay";
+    let buf = fs::read(path).unwrap();
+
+    let needle_start = "[{\"personal\":";
+    let needle_end = "}}]";
+    let idx_start = buf.find(needle_start).expect("not  found");
+    let idx_end = buf[idx_start..].find(needle_end).expect("not  found");
+    println!("start: {idx_start}");
+    println!("end: {idx_end}");
+
+    // for byte in &buf {
+    //     print!("{:02x} ", byte);
+    // }
 }
