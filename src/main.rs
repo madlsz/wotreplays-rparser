41bed92acce461fad718c795150ea017782c9e90
madlsz <dlugosz00@gmail.com>
2024-05-20 14:46:37 +0200

    merge players

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,10 @@ use wotreplays_rparser::cl_args::Args;
 use wotreplays_rparser::load_replay;
 use wotreplays_rparser::player::Player;
 
-fn main() {
-    let args = Args::parse();
+fn get_players(replay: &serde_json::Value, buf: &mut Vec<Player>) {
+    let author_team = replay[0]["personal"]["avatar"]["team"].as_u64().unwrap();
+    let battle_winner = replay[0]["common"]["winnerTeam"].as_u64().unwrap();
 
-    let path = "20240519_2045_poland-Pl21_CS_63_05_prohorovka.wotreplay";
-    let replay = load_replay(&path).unwrap();
-
-    let author_team = &replay[0]["personal"]["avatar"]["team"];
-    let battle_winner = &replay[0]["common"]["winnerTeam"];
-
-    let mut players: Vec<Player> = Vec::new();
     for (rep_id, _) in replay[1].as_object().unwrap().iter() {
         let path = &replay[1][rep_id];
 
@@ -30,12 +24,12 @@ fn main() {
             0 => false,
             _ => true,
         };
-        let max_health = path["maxHealth"].as_u64().unwrap();
+        // let max_health = path["maxHealth"].as_u64().unwrap();
         let clan_abbrev = path["clanAbbrev"].as_str().unwrap();
-        let is_team_killer = match &path["isTeamKiller"].as_u64().unwrap() {
-            0 => false,
-            _ => true,
-        };
+        // let is_team_killer = match &path["isTeamKiller"].as_u64().unwrap() {
+        //     0 => false,
+        //     _ => true,
+        // };
         let team = if &path["team"] == battle_winner {
             "green"
         } else {
@@ -43,26 +37,234 @@ fn main() {
         };
         let avatar_session_id = path["avatarSessionID"].as_str().unwrap();
 
-        players.push(Player {
+        let path = &replay[0]["vehicles"][avatar_session_id][0];
+
+        let account_dbid = path["accountDBID"].as_u64().unwrap();
+        let spotted = path["spotted"].as_u64().unwrap();
+        let vehicle_num_captured = path["vehicleNumCaptured"].as_u64().unwrap();
+        let damage_assisted_track = path["damageAssistedTrack"].as_u64().unwrap();
+        let xp_penalty = path["xpPenalty"].as_u64().unwrap();
+        let direct_team_hits = path["directTeamHits"].as_u64().unwrap();
+        let damage_received = path["damageReceived"].as_u64().unwrap();
+        let life_time = path["lifeTime"].as_u64().unwrap();
+        let piercings_received = path["piercingsReceived"].as_u64().unwrap();
+        let sniper_damage_dealt = path["sniperDamageDealt"].as_u64().unwrap();
+        // let destructible_deaths = path["destructibleDeaths"].as_array().unwrap();
+        let piercing_enemy_hits = path["piercingEnemyHits"].as_u64().unwrap();
+        let damage_assisted_radio = path["damageAssistedRadio"].as_u64().unwrap();
+        let mileage = path["mileage"].as_u64().unwrap();
+        let stun_duration = path["stunDuration"].as_f64().unwrap();
+        let piercings = path["piercings"].as_u64().unwrap();
+        let damage_blocked_by_armor = path["damageBlockedByArmor"].as_u64().unwrap();
+        let xp = path["xp"].as_u64().unwrap();
+        let dropped_capture_points = path["droppedCapturePoints"].as_u64().unwrap();
+        // let killer_id = path["killerID"].as_u64().unwrap();
+        let xp_other = path["xp/other"].as_u64().unwrap();
+        // let index = path["index"].as_u64().unwrap();
+        let direct_hits_received = path["directHitsReceived"].as_u64().unwrap();
+        let damage_received_from_invisibles = replay[0]["vehicles"][avatar_session_id][0]
+            ["damageReceivedFromInvisibles"]
+            .as_u64()
+            .unwrap();
+        let explosion_hits_received = path["explosionHitsReceived"].as_u64().unwrap();
+        let achievement_xp = path["achievementXP"].as_u64().unwrap();
+        // let death_reason = path["deathReason"].as_i64().unwrap();
+        let capture_points = path["capturePoints"].as_u64().unwrap();
+        let num_recovered = path["numRecovered"].as_u64().unwrap();
+        let direct_enemy_hits = path["directEnemyHits"].as_u64().unwrap();
+        // let max_health = path["maxHealth"].as_u64().unwrap();
+        // let damage_event_list = path["damageEventList"].as_u64().unwrap();
+        // let health = path["health"].as_u64().unwrap();
+        // let stop_respawn = path["stopRespawn"].as_bool().unwrap();
+        let achievement_credits = path["achievementCredits"].as_u64().unwrap();
+        // let achievements = path["achievements"].as_array().unwrap();
+        let xp_assist = path["xp/assist"].as_u64().unwrap();
+        let shots = path["shots"].as_u64().unwrap();
+        let kills = path["kills"].as_u64().unwrap();
+        let death_count = path["deathCount"].as_u64().unwrap();
+        let damaged_hp = path["damagedHp"].as_u64().unwrap();
+        let flag_capture = path["flagCapture"].as_u64().unwrap();
+        let damaged = path["damaged"].as_u64().unwrap();
+        let t_damage_dealt = path["tdamageDealt"].as_u64().unwrap();
+        let resource_absorbed = path["resourceAbsorbed"].as_u64().unwrap();
+        let credits = path["credits"].as_u64().unwrap();
+        let account_dbid = path["accountDBID"].as_u64().unwrap();
+        let artillery_fort_equip_damage_dealt = replay[0]["vehicles"][avatar_session_id][0]
+            ["artilleryFortEquipDamageDealt"]
+            .as_u64()
+            .unwrap();
+        let no_damage_direct_hits_received = replay[0]["vehicles"][avatar_session_id][0]
+            ["noDamageDirectHitsReceived"]
+            .as_u64()
+            .unwrap();
+        let num_defended = path["numDefended"].as_u64().unwrap();
+        let stunned = path["stunned"].as_u64().unwrap();
+        let equipment_damage_dealt = path["equipmentDamageDealt"].as_u64().unwrap();
+        // let is_team_killer = path["isTeamKiller"].as_bool().unwrap();
+        // let type_comp_descr = path["typeCompDescr"].as_u64().unwrap();
+        let solo_flag_capture = path["soloFlagCapture"].as_u64().unwrap();
+        let destructibles_hits = path["destructiblesHits"].as_u64().unwrap();
+        // let capturing_base = path["capturingBase"].as_null().unwrap();
+        let damage_assisted_stun = path["damageAssistedStun"].as_u64().unwrap();
+        let rollouts_count = path["rolloutsCount"].as_u64().unwrap();
+        let t_kills = path["tkills"].as_u64().unwrap();
+        let potential_damage_received = path["potentialDamageReceived"].as_u64().unwrap();
+        let damage_dealt = path["damageDealt"].as_u64().unwrap();
+        let destructibles_num_destroyed = replay[0]["vehicles"][avatar_session_id][0]
+            ["destructiblesNumDestroyed"]
+            .as_u64()
+            .unwrap();
+        let damage_assisted_smoke = path["damageAssistedSmoke"].as_u64().unwrap();
+        let destructibles_damage_dealt = replay[0]["vehicles"][avatar_session_id][0]
+            ["destructiblesDamageDealt"]
+            .as_u64()
+            .unwrap();
+        // let flag_actions = path["flagActions"].as_array().unwrap();
+        let win_points = path["winPoints"].as_u64().unwrap();
+        let explosion_hits = path["explosionHits"].as_u64().unwrap();
+        // let team = path["team"].as_u64().unwrap();
+        let xp_attack = path["xp/attack"].as_u64().unwrap();
+        let t_destroyed_modules = path["tdestroyedModules"].as_u64().unwrap_or(0u64);
+        let stun_num = path["stunNum"].as_u64().unwrap();
+        let damage_assisted_inspire = path["damageAssistedInspire"].as_u64().unwrap();
+        // let in_battle_achievements = path["inBattleAchievements"].as_array().unwrap();
+        let achievement_free_xp = path["achievementFreeXP"].as_u64().unwrap();
+        let direct_hits = path["directHits"].as_u64().unwrap();
+
+        let opposite_team = if author_team == 1u64 { 2u64 } else { 1u64 };
+
+        let team = if team == "green" {
+            author_team
+        } else {
+            opposite_team
+        };
+        let mut battles_won = 0;
+        let mut battles_lost = 0;
+        let mut battles_drew = 0;
+        if battle_winner == team {
+            battles_won += 1u64;
+        } else if [1u64, 2u64].contains(&battle_winner) {
+            battles_lost += 1u64;
+        } else {
+            battles_drew += 1u64;
+        };
+        let clan_id = replay[0]["players"][&account_dbid.to_string()]["clanDBID"]
+            .as_u64()
+            .unwrap();
+
+        buf.push(Player {
+            account_dbid,
+            battles_played: 1u64,
             vehicle_type: vehicle_type.as_str().unwrap().to_string(),
             name: name.to_string(),
             fake_name: fake_name.to_string(),
             wtr,
-            is_alive,
-            max_health,
+            battles_survived: if is_alive { 1u64 } else { 0u64 },
             clan_abbrev: clan_abbrev.to_string(),
-            is_team_killer,
-            team: team.to_string(),
             avatar_session_id: avatar_session_id.to_string(),
+            spotted,
+            vehicle_num_captured,
+            damage_assisted_track,
+            xp_penalty,
+            direct_team_hits,
+            damage_received,
+            life_time,
+            piercings_received,
+            sniper_damage_dealt,
+            piercing_enemy_hits,
+            damage_assisted_radio,
+            mileage,
+            stun_duration,
+            piercings,
+            damage_blocked_by_armor,
+            xp,
+            dropped_capture_points,
+            // killer_id,
+            xp_other,
+            // index,
+            direct_hits_received,
+            damage_received_from_invisibles,
+            explosion_hits_received,
+            achievement_xp,
+            // death_reason,
+            capture_points,
+            num_recovered,
+            direct_enemy_hits,
+            // max_health,
+            // health,
+            // stop_respawn,
+            achievement_credits,
+            xp_assist,
+            shots,
+            kills,
+            death_count,
+            damaged_hp,
+            flag_capture,
+            damaged,
+            t_damage_dealt,
+            resource_absorbed,
+            credits,
+            artillery_fort_equip_damage_dealt,
+            no_damage_direct_hits_received,
+            num_defended,
+            stunned,
+            equipment_damage_dealt,
+            // is_team_killer,
+            // type_comp_descr,
+            solo_flag_capture,
+            destructibles_hits,
+            damage_assisted_stun,
+            rollouts_count,
+            t_kills,
+            potential_damage_received,
+            damage_dealt,
+            destructibles_num_destroyed,
+            damage_assisted_smoke,
+            destructibles_damage_dealt,
+            win_points,
+            explosion_hits,
+            xp_attack,
+            t_destroyed_modules,
+            stun_num,
+            damage_assisted_inspire,
+            achievement_free_xp,
+            direct_hits,
+            opposite_team,
+            team,
+            battles_won,
+            battles_lost,
+            battles_drew,
+            clan_id,
         });
+    }
+}
 
-        let path = &replay[0]["vehicles"][avatar_session_id][0];
+fn merge_players(players: &[Player]) -> Vec<Player> {
+    let mut merged_players = Vec::new();
 
-        let spotted = path["spotted"].as_u64().unwrap();
+    for player in players {
+        match merged_players.iter().position(|p| p == player) {
+            Some(i) => merged_players[i] += player.clone(),
+            None => merged_players.push(player.clone()),
+        };
     }
 
-    println!("{players:?}");
-    // for byte in &buf {
-    //     print!("{:02x} ", byte);
-    // }
+    merged_players
+}
+
+fn main() {
+    // let args = Args::parse();
+
+    let replays = [
+        "20240519_2045_poland-Pl21_CS_63_05_prohorovka.wotreplay",
+        "20240519_2300_uk-GB91_Super_Conqueror_10_hills.wotreplay",
+    ];
+    let mut buf = Vec::new();
+    for path in replays {
+        let replay = load_replay(&path).unwrap();
+        get_players(&replay, &mut buf);
+    }
+
+    let merged_players = merge_players(&buf);
+    println!("{} -> {}", buf.len(), merged_players.len());
 }
--- src/player.rs
+++ src/player.rs
@@ -1,13 +1,163 @@
-#[derive(Debug)]
+use std::cmp;
+use std::ops;
+
+#[derive(Debug, Clone)]
 pub struct Player {
+    pub account_dbid: u64,
+    pub battles_played: u64,
     pub vehicle_type: String,
     pub name: String,
     pub fake_name: String,
     pub wtr: u64,
-    pub is_alive: bool,
-    pub max_health: u64,
+    pub battles_survived: u64,
     pub clan_abbrev: String,
-    pub is_team_killer: bool,
-    pub team: String,
     pub avatar_session_id: String,
+    pub spotted: u64,
+    pub vehicle_num_captured: u64,
+    pub damage_assisted_track: u64,
+    pub xp_penalty: u64,
+    pub direct_team_hits: u64,
+    pub damage_received: u64,
+    pub life_time: u64,
+    pub piercings_received: u64,
+    pub sniper_damage_dealt: u64,
+    pub piercing_enemy_hits: u64,
+    pub damage_assisted_radio: u64,
+    pub mileage: u64,
+    pub stun_duration: f64,
+    pub piercings: u64,
+    pub damage_blocked_by_armor: u64,
+    pub xp: u64,
+    pub dropped_capture_points: u64,
+    // pub killer_id: u64,
+    pub xp_other: u64,
+    // pub index: u64,
+    pub direct_hits_received: u64,
+    pub damage_received_from_invisibles: u64,
+    pub explosion_hits_received: u64,
+    pub achievement_xp: u64,
+    // pub death_reason: i64,
+    pub capture_points: u64,
+    pub num_recovered: u64,
+    pub direct_enemy_hits: u64,
+    // pub max_health: u64,
+    // pub health: u64,
+    // pub stop_respawn: bool,
+    pub achievement_credits: u64,
+    pub xp_assist: u64,
+    pub shots: u64,
+    pub kills: u64,
+    pub death_count: u64,
+    pub damaged_hp: u64,
+    pub flag_capture: u64,
+    pub damaged: u64,
+    pub t_damage_dealt: u64,
+    pub resource_absorbed: u64,
+    pub credits: u64,
+    pub artillery_fort_equip_damage_dealt: u64,
+    pub no_damage_direct_hits_received: u64,
+    pub num_defended: u64,
+    pub stunned: u64,
+    pub equipment_damage_dealt: u64,
+    // pub is_team_killer: bool,
+    // pub type_comp_descr: u64,
+    pub solo_flag_capture: u64,
+    pub destructibles_hits: u64,
+    pub damage_assisted_stun: u64,
+    pub rollouts_count: u64,
+    pub t_kills: u64,
+    pub potential_damage_received: u64,
+    pub damage_dealt: u64,
+    pub destructibles_num_destroyed: u64,
+    pub damage_assisted_smoke: u64,
+    pub destructibles_damage_dealt: u64,
+    pub win_points: u64,
+    pub explosion_hits: u64,
+    pub xp_attack: u64,
+    pub t_destroyed_modules: u64,
+    pub stun_num: u64,
+    pub damage_assisted_inspire: u64,
+    pub achievement_free_xp: u64,
+    pub direct_hits: u64,
+    pub opposite_team: u64,
+    pub team: u64,
+    pub battles_won: u64,
+    pub battles_lost: u64,
+    pub battles_drew: u64,
+    pub clan_id: u64,
+}
+
+impl cmp::PartialEq for Player {
+    fn eq(&self, other: &Self) -> bool {
+        self.account_dbid == other.account_dbid
+    }
+}
+
+impl ops::AddAssign for Player {
+    fn add_assign(&mut self, rhs: Self) {
+        self.battles_played += rhs.battles_played;
+        self.battles_survived += rhs.battles_survived;
+        self.spotted += rhs.spotted;
+        self.vehicle_num_captured += rhs.vehicle_num_captured;
+        self.damage_assisted_track += rhs.damage_assisted_track;
+        self.xp_penalty += rhs.xp_penalty;
+        self.direct_team_hits += rhs.direct_team_hits;
+        self.damage_received += rhs.damage_received;
+        self.life_time += rhs.life_time;
+        self.piercings_received += rhs.piercings_received;
+        self.sniper_damage_dealt += rhs.sniper_damage_dealt;
+        self.piercing_enemy_hits += rhs.piercing_enemy_hits;
+        self.damage_assisted_radio += rhs.damage_assisted_radio;
+        self.mileage += rhs.mileage;
+        self.stun_duration += rhs.stun_duration;
+        self.piercings += rhs.piercings;
+        self.damage_blocked_by_armor += rhs.damage_blocked_by_armor;
+        self.xp += rhs.xp;
+        self.dropped_capture_points += rhs.dropped_capture_points;
+        self.xp_other += rhs.xp_other;
+        self.direct_hits_received += rhs.direct_hits_received;
+        self.damage_received_from_invisibles += rhs.damage_received_from_invisibles;
+        self.explosion_hits_received += rhs.explosion_hits_received;
+        self.achievement_xp += rhs.achievement_xp;
+        self.capture_points += rhs.capture_points;
+        self.num_recovered += rhs.num_recovered;
+        self.direct_enemy_hits += rhs.direct_enemy_hits;
+        self.achievement_credits += rhs.achievement_credits;
+        self.xp_assist += rhs.xp_assist;
+        self.shots += rhs.shots;
+        self.kills += rhs.kills;
+        self.death_count += rhs.death_count;
+        self.damaged_hp += rhs.damaged_hp;
+        self.flag_capture += rhs.flag_capture;
+        self.damaged += rhs.damaged;
+        self.t_damage_dealt += rhs.t_damage_dealt;
+        self.resource_absorbed += rhs.resource_absorbed;
+        self.credits += rhs.credits;
+        self.artillery_fort_equip_damage_dealt += rhs.artillery_fort_equip_damage_dealt;
+        self.no_damage_direct_hits_received += rhs.no_damage_direct_hits_received;
+        self.num_defended += rhs.num_defended;
+        self.stunned += rhs.stunned;
+        self.equipment_damage_dealt += rhs.equipment_damage_dealt;
+        self.solo_flag_capture += rhs.solo_flag_capture;
+        self.destructibles_hits += rhs.destructibles_hits;
+        self.damage_assisted_stun += rhs.damage_assisted_stun;
+        self.rollouts_count += rhs.rollouts_count;
+        self.t_kills += rhs.t_kills;
+        self.potential_damage_received += rhs.potential_damage_received;
+        self.damage_dealt += rhs.damage_dealt;
+        self.destructibles_num_destroyed += rhs.destructibles_num_destroyed;
+        self.damage_assisted_smoke += rhs.damage_assisted_smoke;
+        self.destructibles_damage_dealt += rhs.destructibles_damage_dealt;
+        self.win_points += rhs.win_points;
+        self.explosion_hits += rhs.explosion_hits;
+        self.xp_attack += rhs.xp_attack;
+        self.t_destroyed_modules += rhs.t_destroyed_modules;
+        self.stun_num += rhs.stun_num;
+        self.damage_assisted_inspire += rhs.damage_assisted_inspire;
+        self.achievement_free_xp += rhs.achievement_free_xp;
+        self.direct_hits += rhs.direct_hits;
+        self.battles_won += rhs.battles_won;
+        self.battles_lost += rhs.battles_lost;
+        self.battles_drew += rhs.battles_drew;
+    }
 }
