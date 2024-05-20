91a88a2c0a3ecfca91b6707ef4cd3933ad80f6a6
madlsz <dlugosz00@gmail.com>
2024-05-20 03:09:34 +0200

    unwrapping values

--- src/main.rs
+++ src/main.rs
@@ -21,15 +21,15 @@ fn main() {
         {
             continue;
         }
-        let name = &replay[1][rep_id]["name"];
-        let fake_name = &replay[1][rep_id]["fakeName"];
-        let wtr = &replay[1][rep_id]["wtr"];
-        let is_alive = match &replay[1][rep_id]["isAlive"].as_u64().unwrap() {
+        let name = replay[1][rep_id]["name"].as_str().unwrap();
+        let fake_name = replay[1][rep_id]["fakeName"].as_str().unwrap();
+        let wtr = replay[1][rep_id]["wtr"].as_u64().unwrap_or(0u64);
+        let is_alive = match replay[1][rep_id]["isAlive"].as_u64().unwrap() {
             0 => false,
             _ => true,
         };
-        let max_health = &replay[1][rep_id]["maxHealth"];
-        let clan_abbrev = &replay[1][rep_id]["clanAbbrev"];
+        let max_health = replay[1][rep_id]["maxHealth"].as_u64().unwrap();
+        let clan_abbrev = replay[1][rep_id]["clanAbbrev"].as_str().unwrap();
         let is_team_killer = match &replay[1][rep_id]["isTeamKiller"].as_u64().unwrap() {
             0 => false,
             _ => true,
@@ -39,20 +39,22 @@ fn main() {
         } else {
             "red"
         };
-        let avatar_session_id = &replay[1][rep_id]["avatarSessionID"];
+        let avatar_session_id = replay[1][rep_id]["avatarSessionID"].as_str().unwrap();
 
         players.push(Player {
             vehicle_type: vehicle_type.as_str().unwrap().to_string(),
-            name: name.as_str().unwrap().to_string(),
-            fake_name: fake_name.as_str().unwrap().to_string(),
-            wtr: wtr.as_u64().unwrap_or(0u64),
+            name: name.to_string(),
+            fake_name: fake_name.to_string(),
+            wtr,
             is_alive,
-            max_health: max_health.as_u64().unwrap(),
-            clan_abbrev: clan_abbrev.as_str().unwrap().to_string(),
+            max_health,
+            clan_abbrev: clan_abbrev.to_string(),
             is_team_killer,
             team: team.to_string(),
-            avatar_session_id: avatar_session_id.as_str().unwrap().to_string(),
+            avatar_session_id: avatar_session_id.to_string(),
         });
+
+        let spotted = &replay[0]["vehicles"][avatar_session_id][0]["spotted"];
     }
 
     println!("{players:?}");
